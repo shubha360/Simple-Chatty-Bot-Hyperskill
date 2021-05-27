bc541ba66bea7e40859472ca22aa8fc028a812f9
Raquibul Islam <shubha360@outlook.com>
2021-05-27 16:44:17 +0600

    Complete project

---
+++ Stage 1 - Chatty Bot welcomes you/SimpleBot.java
@@ -0,0 +1,10 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        // write your code here
+        
+        System.out.println("Hello! My name is Cyber.");
+        System.out.println("I was created in 2021.");
+    }
+}
